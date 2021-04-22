3c0e2db18e290554c133bd53f3d9521fec2d5611
Rodrigo Rodrigues <rodrigo.oliveira@brmalls.com.br>
2021-04-22 16:29:03 -0300

    ajuste projeto

--- src/main/java/br/com/campanha/domain/repository/SocioTorcedorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.campanha.domain.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.campanha.domain.entity.SocioTorcedorEntity;
-
-public interface SocioTorcedorRepository extends CrudRepository<SocioTorcedorEntity, Long> {
-
-}
